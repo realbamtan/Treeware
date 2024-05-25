fbdd91eb67fdea1d3bf04f628797e3af47c626f3
realbamtan <ganasmain333@gmail.com>
2024-05-25 17:51:34 +0800

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,7 +1,7 @@
 main() {
     clear
     echo -e "Executed"
-    DYLD_INSERT_LIBRARIES="/Applications/Roblox.app/Contents/MacOS/Treeware.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" command > /dev/null 2>&1
+    /Applications/Roblox.app/Contents/MacOS/RobloxPlayer -o command > /dev/null 2>&1
     rm wget-log
     exit
 }
