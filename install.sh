8cc903f3f768b010c0aead64f21f720cb9414eeb
realbamtan <ganasmain333@gmail.com>
2024-05-25 18:17:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ main() {
     echo -e "Patching Roblox..."
     mv ./Treeware.dylib "/Applications/Roblox.app/Contents/MacOS/Treeware.dylib"
     wget "https://github.com/realbamtan/Treeware/raw/main/insert" 
+    chmod +x insert
     ./insert "/Applications/Roblox.app/Contents/MacOS/Treeware.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes
     mv "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer"
     echo -e "batman will keep u all safe now CLOSE this terminal, open a NEW one and RUN the execute cmd"
