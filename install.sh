34423820e5e403e8180fee675a222fda0c510be3
realbamtan <ganasmain333@gmail.com>
2024-05-25 12:37:00 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ main() {
     clear
     echo -e "GEX Printware installing working as of 25/05/24"
     echo -e "GEX Version 0.0"
-    curl "https://github.com/realbamtan/Treeware/blob/main/Treeware.dylib" 
+    curl "https://github.com/realbamtan/Treeware/raw/main/Treeware.dylib" 
     echo -e " Done."
     echo -e "Patching Roblox..."
     mv ./Treeware.dylib "/Applications/Roblox.app/Contents/MacOS/Treeware.dylib"
